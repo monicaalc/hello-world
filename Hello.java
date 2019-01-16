b9edd8fcc9c7b3ac45475671e6b1ec97f92487ae
monicaalc <46757011+monicaalc@users.noreply.github.com>
2019-01-16 12:22:05 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/*************************************
+* Java Program name
+**************************************
+* Project/program description
+*-----------------------------------
+* Monica Alcoba
+* 01/16/19
+*CMCS255 004
+***************************************/
+
+public class Hello {
+	public static void printHeading (){
+	System.out.println("Monica Alcoba");
+	System.out.println("Hello Project");
+	System.out.println("Introducting myself");
+	System.out.println("01/16/19");
+	System.out.println("CMCS255 004");
+	System.out.println();
+	}
+
+	public static void main(String [] args){
+	printHeading();
+	System.out.println("Hello");
+	}
+}
+
